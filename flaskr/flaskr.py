86837d5db280add2146c37a13741e249e25a3417
John Kwening <kwening.john@gmail.com>
2017-10-27 23:04:55 -0400

    created the database

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -2,6 +2,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -18,6 +19,14 @@ app.config.from_object(__name__)
 # used to open a connection on request
 def connect_db():
     return sqlite3.connect(database=app.config['DATABASE'])
+
+
+# initializes the database
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource(resource='schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 # fires up the server if we want to run as standalone app

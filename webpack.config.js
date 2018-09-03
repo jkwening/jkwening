9e0610bd9d6176740ea5ddb149b5658bc5eee1bf
John Kwening <kwening.john@gmail.com>
2018-09-03 12:21:10 -0400

    chore: Configure babel
    Install modules needed for babel and interaction with react. Configure
    webpack and babelrc to use presets and plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true
+        }
+      }
+    ]
   }
 };
